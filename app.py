759c1de2c6fbfa464a8342c0d1d21436c63ad4f8
ivanqo <ivan.basenko.05@mail.ru>
2025-10-02 03:21:04 +0300

    изменил app.py

--- app.py
+++ app.py
@@ -304,5 +304,5 @@ async def predict(req: PredictRequest, request: Request):
         if label != "O"
     ]
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+
+uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
